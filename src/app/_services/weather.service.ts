7fa2c866ef8e7039c132136f2c561ffa1a04828e
asawalhi <sawalhi.ahmad@gmail.com>
2021-04-09 20:09:00 -0500

    Weather API with live location

--- src/app/_services/weather.service.ts
+++ src/app/_services/weather.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,16 +13,27 @@ export class WeatherService {
   units: string;
   url! : string;
   forecast: any;
+  ipv4:any
+  backendUrl: any
+  backendres : any
+  userCity: any
+  userCountry: any
   
   constructor(private http: HttpClient) {
     this.baseUrl = 'http://api.openweathermap.org/data/2.5/';
+    this.backendUrl = 'http://localhost:2900/'
     this.appId = 'b5ea0f92c5b8deb13ccec7d557ac6a4d';
     this.units = 'metric';
    }
 
-   getWeatherForecast(): Observable<any> {
+   
+   getLocation(){
+    return this.http.get(this.backendUrl)
+   }
+   
+   getWeatherForecast(cx:string): Observable<any> {
      
-    this.url = this.baseUrl + 'forecast?q=dallas' + '&appid=' + this.appId + '&units=' + this.units  ;     
+    this.url = this.baseUrl + 'forecast?q=' + cx + '&appid=' + this.appId + '&units=' + this.units  ;     
     return this.http.get(this.url)
    //return this.forecast;
      //.catch(this.handleError);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,20 +10,33 @@ import { WeatherService } from '../_services/weather.service';
 export class HomeComponent implements OnInit {
 
   weatherForecastData: any;
+  userLocation: any
   errorMessage: any
+
+
 
 
 
   constructor(private _weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    this._weatherService.getWeatherForecast()
+    this._weatherService.getLocation()
     .subscribe(data => { 
+      this.userLocation = data
+      console.log(this.userLocation)
+      
+      this._weatherService.getWeatherForecast(this.userLocation.city)
+      .subscribe(data => { 
       this.weatherForecastData = data
       console.log("this.weatherForecastData = "+this.weatherForecastData)
-    }, error => 
-    this.errorMessage = <any>error 
-    );
+      }, error => 
+      this.errorMessage = <any>error 
+      );
+    
+    })
+
+
+    
   }
 
 }
