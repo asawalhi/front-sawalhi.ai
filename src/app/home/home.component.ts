2cdde4e9d8f988e7819b06d4c3fdcb8ee042f7fe
asawalhi <sawalhi.ahmad@gmail.com>
2021-04-09 20:57:47 -0500

    Weather API with live location with Sports API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../_services/weather.service';
+import { SportsService } from '../_services/sports.service'
 
 
 @Component({
@@ -12,12 +13,23 @@ export class HomeComponent implements OnInit {
   weatherForecastData: any;
   userLocation: any
   errorMessage: any
+  sportsView = false
+  firstSportsType: any
+  secondSportsType: any
+  thirdSportsType: any
+  firstSportsTypeDtlsView = false
+  secondSportsTypeDtlsView = false
+  thirdSportsTypeDtlsView = false
+  sportTypes: any
+  aboutusView: any
+  contactusView: any
+  homeView= true
 
 
 
 
 
-  constructor(private _weatherService: WeatherService) { }
+  constructor(private _weatherService: WeatherService, private _sportsService: SportsService) { }
 
   ngOnInit(): void {
     this._weatherService.getLocation()
@@ -35,8 +47,47 @@ export class HomeComponent implements OnInit {
     
     })
 
+  }
 
-    
+  //View control
+  openHomeView(){
+    this.sportsView = false
+    this.aboutusView = false
+    this.contactusView = false
+    this.homeView = true
+  }
+
+  openSportsView(){
+    this.aboutusView = false
+    this.contactusView = false
+    this.homeView = true
+    this.sportsView = true
+
+    let randomIndex1 = Math.floor(Math.random() * 11);
+    let randomIndex2 = Math.floor(Math.random() * 12);
+    let randomIndex3 = Math.floor(Math.random() * 9);
+
+    this._sportsService.getSportTypes().subscribe(data=>{
+      this._sportsService.sportsCollection = JSON.stringify(data)
+      this.sportTypes = data
+      console.log("this.sportTypes = data" + data)
+      this.firstSportsType = this.sportTypes.sports[randomIndex1]
+      this.secondSportsType = this.sportTypes.sports[randomIndex2]
+      this.thirdSportsType = this.sportTypes.sports[randomIndex3]
+      
+    })
+  }
+
+  openfirstSportsTypeDtls(){
+    this.firstSportsTypeDtlsView = true
+  }
+
+  opensecondSportsTypeDtls(){
+    this.secondSportsTypeDtlsView = true
+  }
+
+  openthirdSportsTypeDtls(){
+    this.thirdSportsTypeDtlsView = true
   }
 
 }
